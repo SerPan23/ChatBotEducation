4a9e8d0a7f1eaf032eb30e88f4a99cc997cf68d6
SerPan23 <igracom@yandex.ru>
2020-09-04 20:39:36 +0300

    u

--- kb.py
+++ kb.py
@@ -1,4 +1,18 @@
 from telebot import types
+import db
+
+
+directListKb = types.InlineKeyboardMarkup()  # наша клавиатура
+key_math = types.InlineKeyboardButton(text='Математика', callback_data='math')
+directListKb.add(key_math)  # добавляем кнопку в клавиатуру
+
+
+topicsListKb = types.InlineKeyboardMarkup()
+key_quadratic_equation = types.InlineKeyboardButton(text='Квадратное уравнение',
+                                                            callback_data='quadratic_equation')
+topicsListKb.add(key_quadratic_equation)  # добавляем кнопку в клавиатуру
+
+
 nextBackKb = types.ReplyKeyboardMarkup(row_width=2)
 itembtn1 = types.KeyboardButton('Следующее задание')
 itembtn2 = types.KeyboardButton("Назад")
@@ -11,3 +25,9 @@ itembtn32 = types.KeyboardButton("Назад")
 retryKb.add(itembtn12, itembtn22, itembtn32)
 
 markupClose = types.ReplyKeyboardRemove(selective=False)
+
+answerKb = types.ReplyKeyboardMarkup(row_width=2)
+answbtn1 = types.KeyboardButton(db.tasks[db.taskId][1])
+answbtn2 = types.KeyboardButton(db.tasks[db.taskId][2])
+answbtn3 = types.KeyboardButton(db.tasks[db.taskId][3])
+answerKb.add(answbtn1, answbtn2, answbtn3)
--- main.py
+++ main.py
@@ -7,42 +7,25 @@ from telebot import types
 bot = telebot.TeleBot(se.TOKEN)
 
 
-tasks = [['3x^2 - 14x - 5 = 0', '5, 1/3', '10, 5/8', '1/9, 0'], ['x^2 - x + 5 = 0', '2', '6', '10']]
-
-
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
-    key_math = types.InlineKeyboardButton(text='Математика', callback_data='math')
-    keyboard.add(key_math)  # добавляем кнопку в клавиатуру
     bot.send_message(message.from_user.id,
                      "Привет, меня зовут E-Bot, я направлен помочь тебе в изучении школьной программы.")
     bot.send_message(message.from_user.id,
-                     "Выбери предмет:", reply_markup=keyboard)
+                     "Выбери предмет:", reply_markup=kb.directListKb)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "math":
-        keyboard = types.InlineKeyboardMarkup()
-        key_quadratic_equation = types.InlineKeyboardButton(text='Квадратное уравнение',
-                                                            callback_data='quadratic_equation')
-        keyboard.add(key_quadratic_equation)  # добавляем кнопку в клавиатуру
-        bot.send_message(call.message.chat.id, 'Выбери задание:', reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, 'Выбери задание:', reply_markup=kb.topicsListKb)
     elif call.data == "quadratic_equation":
-        print(db.taskId)
-        markup = types.ReplyKeyboardMarkup(row_width=2)
-        itembtn1 = types.KeyboardButton(tasks[db.taskId][1])
-        itembtn2 = types.KeyboardButton(tasks[db.taskId][2])
-        itembtn3 = types.KeyboardButton(tasks[db.taskId][3])
-        markup.add(itembtn1, itembtn2, itembtn3)
-        bot.send_message(call.message.chat.id, text=tasks[db.taskId][0], reply_markup=markup)
+        bot.send_message(call.message.chat.id, text=db.tasks[db.taskId][0], reply_markup=kb.answerKb)
         bot.register_next_step_handler(call.message, get_answer_for_task)
 
 
 def get_answer_for_task(message):
-    if message.text == tasks[db.taskId][1]:
+    if message.text == db.tasks[db.taskId][1]:
         bot.send_message(message.from_user.id, text='Правильно',
                          reply_markup=kb.nextBackKb)
     else:
@@ -57,22 +40,12 @@ def nextQuestion(message):
             db.taskId += 1
         else:
             db.taskId = 0
-        markup = types.ReplyKeyboardMarkup(row_width=2)
-        itembtn1 = types.KeyboardButton(tasks[db.taskId][1])
-        itembtn2 = types.KeyboardButton(tasks[db.taskId][2])
-        itembtn3 = types.KeyboardButton(tasks[db.taskId][3])
-        markup.add(itembtn1, itembtn2, itembtn3)
-        bot.send_message(message.from_user.id, text=tasks[db.taskId][0],
-                         reply_markup=markup)
+        bot.send_message(message.from_user.id, text=db.tasks[db.taskId][0],
+                         reply_markup=kb.answerKb)
         bot.register_next_step_handler(message, get_answer_for_task)
     elif message.text == 'Повторить':
-        markup = types.ReplyKeyboardMarkup(row_width=2)
-        itembtn1 = types.KeyboardButton(tasks[db.taskId][1])
-        itembtn2 = types.KeyboardButton(tasks[db.taskId][2])
-        itembtn3 = types.KeyboardButton(tasks[db.taskId][3])
-        markup.add(itembtn1, itembtn2, itembtn3)
-        bot.send_message(message.from_user.id, text=tasks[db.taskId][0],
-                         reply_markup=markup)
+        bot.send_message(message.from_user.id, text=db.tasks[db.taskId][0],
+                         reply_markup=kb.answerKb)
         bot.register_next_step_handler(message, get_answer_for_task)
 
 
