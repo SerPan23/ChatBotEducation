793530cd2fa18edf5e1cafb941bc1560ce3fbfd9
mnklop <44546223+mnklop@users.noreply.github.com>
2020-09-04 23:16:58 +0300

    Update func.py

--- func.py
+++ func.py
@@ -24,7 +24,7 @@ def get_answer_for_task(message):
         bot.send_message(message.chat.id, text='Правильно',
                          reply_markup=kb.nextBackKb)
     else:
-        bot.send_message(message.chat.id, text='Неправильно',
+        bot.send_message(message.chat.id, text='хуета',
                          reply_markup=kb.retryKb)
 
 
