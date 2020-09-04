7a824653506ddc40b8817282a3c6cff0e0a2e763
SerPan23 <igracom@yandex.ru>
2020-09-04 23:05:44 +0300

    u

--- func.py
+++ func.py
@@ -16,12 +16,10 @@ def chose_topics(message):
 
 
 def send_task(message):
-    print(db.taskId)
     return bot.send_message(message.chat.id, text=db.tasks[db.taskId][0], reply_markup=kb.get_akb())
 
 
 def get_answer_for_task(message):
-    print('test')
     if message.text == db.tasks[db.taskId][1]:
         bot.send_message(message.chat.id, text='Правильно',
                          reply_markup=kb.nextBackKb)
