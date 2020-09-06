3c30243495d89d3009faf004d9851a42c3a3a83c
SerPan23 <igracom@yandex.ru>
2020-09-06 19:20:45 +0300

    u

--- settings.py
+++ settings.py
@@ -1,1 +1,4 @@
 TOKEN = '1300854152:AAGUe59t9-u1DuOGhRJkWUcKyTOH1TPz0K0'
+
+MONGODB_LINK = 'mongodb+srv://led1158:<1158led>@e-bot.u5frz.mongodb.net/<ebot>?retryWrites=true&w=majority'
+MONGO_DB = 'ebot'
